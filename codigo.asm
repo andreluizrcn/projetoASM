bb93878dc3beb9312b78151892373d75b4c4bfee
andreluizrcn <andreluizrodriguescastro@gmail.com>
2023-10-30 00:06:48 -0300

    commit

--- codigo.asm
+++ codigo.asm
@@ -1,4 +1,4 @@
-%include 'bibliotecaE.inc'
+;include 'bibliotecaE.inc' // USEI MAIS COMO REFERENCIA DO QUE COLOCAR NOS REGISTRADORES
 
 extern printf, scanf
 global main
@@ -118,86 +118,55 @@ main:
     mov edx, 54
     int 80h
 
-    ; Passo 3: Leitura e Escrita dos Pixels
-    censurar:
-    ; Largura fixa do bloco de leitura (6480 bytes)
-    mov ecx, 6480
-
+    censura:
+    mov eax, 3 ; Ler arquivo de entrada
+    mov ebx, [fileHandle]
+    mov ecx, fileBuffer
+    mov edx, 3  ; Largura de 3 bytes por pixel
+    int 80h
+ 
+    mov ebx, fileBuffer  ; ebx aponta para os bytes da imagem
+    mov edx, [integer_x]  ; Coordenada X inicial
+    mov esi, [integer_y]  ; Coordenada Y inicial
+    mov edi, [integer_width]  ; Largura da censura
+    mov ebp, [integer_height]
+ 
+            
+;censurando linha por linha
     linha_loop:
-        ; Ler um bloco de pixels
-        mov eax, 3 ; Ler arquivo de entrada
-        mov ebx, [fileHandle]
-        mov edx, ecx
-        int 80h
+    mov eax, 3
+    mov ebx, [fileHandle]
+    mov ecx, fileBuffer 
 
-        ; Aqui, você deve implementar a lógica para censurar a região retangular.
-        ; Percorra os bytes lidos (representando pixels) e aplique a censura na área especificada.
-        
-        ; Registro ECX deve conter o número de bytes lidos no bloco
-        
-        censurar_pixels:
-            ; Verifique se a posição atual (posição_x, posição_y) está dentro da área a ser censurada
-            ; Lembre-se de que as coordenadas crescem da esquerda para a direita e de baixo para cima em arquivos BMP.
-            ; Portanto, X=250 e Y=310 representam o canto superior esquerdo da área a ser censurada.
-            ; Largura=230 e altura=30
-            
-            ; Calcule a posição atual em relação ao início do bloco
-            ; Suponha que a imagem tenha uma profundidade de cor de 24 bits (3 bytes por pixel).
-            ; A largura da imagem é importante para calcular o deslocamento.
+    mov edx, [integer_width]
+    mov esi, 3
+    mov edi, 0 
+     
+    censurar_linha:
+    mov byte [ecx], 0  ; Censura o componente azul
+    mov byte [ecx + 1], 0  ; Censura o componente verde
+    mov byte [ecx + 2], 0  ; Censura o componente vermelho
+    add ecx, 3  ; Avança para o próximo pixel
+    sub edx, 1  ; Decrementa a largura da censura
+    cmp edx, 0
+    je linha_loop
+    jmp censurar_linha
 
-            ; Deslocamento atual no bloco
-            mov esi, ecx
-            sub esi, edx
+add esi, 1  ; Avança para a próxima linha
+sub ebp, 1  ; Decrementa a altura da censura
+cmp ebp, 0
+je fim
+jmp linha_loop
 
-            ; Calcule a posição_x e posição_y dentro da imagem completa
-            mov eax, [integer_x]
-            add eax, esi
-            mov ebx, [integer_y]
-            add ebx, 1 ; Lembre-se de que as coordenadas crescem de baixo para cima
-            sub ebx, ecx
-            sub ebx, edx
 
-            ; Verifique se a posição_x e posição_y estão dentro da área a ser censurada
-            ; X=250, Y=310, largura=230, altura=30
-            mov edx, [integer_width]
-            add edx, 250
-            cmp eax, 250
-            jl pixel_fora_da_area_x
-            cmp eax, edx
-            jge pixel_fora_da_area_x
 
-            mov edx, [integer_height]
-            add edx, 310
-            cmp ebx, 310
-            jl pixel_fora_da_area_y
-            cmp ebx, edx
-            jge pixel_fora_da_area_y
+fechar_arquivo:
+mov eax, 6
+mov ebx, [fileHandle]
+int 80h
 
-            ; Pixel está dentro da área a ser censurada, substitua-o por preto (0, 0, 0)
-            mov byte [ecx], 0 ; Blue
-            mov byte [ecx + 1], 0 ; Green
-            mov byte [ecx + 2], 0 ; Red
 
-            pixel_fora_da_area_y:
-
-            pixel_fora_da_area_x:
-            
-            ; Continue verificando os próximos pixels
-            add ecx, 3
-            loop censurar_pixels
-
-        ; Escrever o bloco de pixels (possivelmente censurados) no arquivo de saída
-        mov eax, 5 ; Abrir arquivo de saída
-        mov ebx, filename_out
-        mov ecx, 1 ; Modo de escrita
-        mov edx, 0o777
-        int 80h
-        mov [fileHandle], eax
-
-        mov eax, 4 ; Escrever no arquivo de saída
-        mov ebx, [fileHandle]
-        mov ecx, buffer ; Coloque aqui os bytes censurados
-        int 80h
-
-        ; Continue lendo e censurando o restante da imagem
-        loop linha_loop
+fim: 
+mov eax, 1
+xor ebx, ebx
+int 80h
