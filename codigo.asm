3a63992661320264315a85fcee9c0f8dbeafadc2
Bruno Luís <99904338+BrunoLuisUFPB@users.noreply.github.com>
2023-11-05 00:00:02 -0300

    Update codigo.asm

--- codigo.asm
+++ codigo.asm
@@ -12,10 +12,11 @@ request_width db "largura do retângulo:  ", 0AH,0H
 request_height db "altura do retângulo:  ", 0AH,0H
 format_string db "%s",0H
 format_num db "%d",0H
-output_filename db "nome_do_arquivo_de_saida.bmp", 0
+;output_filename db "censura.bmp", 0
 
 section .bss
-fileHandle resd 1 ; Handle do arquivo
+fileHandleIn resd 1 ; Handle do arquivo de entrada
+fileHandleOut resd 1 ; Handle do arquivo de saída
 fileBuffer resb 54 ; Buffer para os primeiros 54 bytes do arquivo
 buffer resb 6480
 integer_x resd 1 
@@ -23,7 +24,7 @@ integer_y resd 1
 integer_width resd 1
 integer_height resd 1
 filename_in resb 300
-filename_out resb 300 
+filename_out resb 300
 
 section .text 
 main:
@@ -88,152 +89,97 @@ main:
     call scanf
     add esp, 8
 
-
-    ; Passo 2: Abertura do Arquivo 1 e 2
-    abrir_1:
-    mov eax, 5 ; Abrir arquivo
+    ; Passo 2: Abertura do Arquivo de Entrada
+    abrir_entrada:
+    mov eax, 5 ; Abrir arquivo de entrada
     mov ebx, filename_in
     mov ecx, 0 ; Modo de leitura
-    mov edx, 0777
+    mov ecx, 0o777
     int 80h
-    mov [fileHandle], eax
+    mov dword [fileHandleIn], eax
 
-    ; Ler os primeiros 54 bytes do arquivo
-    mov eax, 3 ; Ler arquivo
-    mov ebx, [fileHandle]
-    mov ecx, fileBuffer
-    mov edx, 54
-    int 80h
-
-    ; Ler e escrever os primeiros 18 bytes (14 + 4) do arquivo
-    mov eax, 3
-    mov ebx, [fileHandle]
-    mov ecx, buffer
-    mov edx, 18
-    int 80h
-    mov eax, 4
-    mov ebx, [fileHandle]
-    mov ecx, buffer
-    mov edx, 18
-    int 80h
-
-    ; Ler 4 bytes referentes à largura da imagem
-    mov eax, 3
-    mov ebx, [fileHandle]
-    mov ecx, integer_width
-    mov edx, 4
-    int 80h
-
-    ; Escrever os 4 bytes no arquivo de saída
-    mov eax, 4
-    mov ebx, [fileHandle]
-    mov ecx, [output_filename]
-    mov edx, 4
-    int 80h
-
-    ; Ler os 32 bytes restantes do cabeçalho da imagem
-    mov eax, 3
-    mov ebx, [fileHandle]
-    mov ecx, buffer
-    mov edx, 32
-    int 80h
-
-    ; Escrever os 32 bytes no arquivo de saída
-    mov eax, 4
-    mov ebx, [fileHandle]
-    mov ecx, buffer
-    mov edx, 32
-    int 80h
-
-    abrir_2: ;arquivo 2
-    ; Escrever os primeiros 54 bytes no arquivo de saída
-    mov eax, 5 ; Abrir arquivo de saída
+    ; Passo 3: Abertura do Arquivo de Saída
+    abrir_saida:
+    mov eax, 8 ; Abrir arquivo de saída
     mov ebx, filename_out
-    mov ecx, 1 ; Modo de escrita
-    mov edx, 0777
+    mov ecx, 0o777 ; Modo de escrita
     int 80h
-    mov [fileHandle], eax
+    mov dword [fileHandleOut], eax
 
-    mov eax, 4 ; Escrever no arquivo de saída
-    mov ebx, [fileHandle]
-    mov ecx, fileBuffer
-    mov edx, 54
-    int 80h
-
-    ; Ler os 32 bytes restantes do cabeçalho da imagem
-    mov eax, 3
-    mov ebx, [fileHandle]
-    mov ecx, buffer
-    mov edx, 32
-    int 80h
-
-    censura:
+    ; Passo 4: Ler os primeiros 54 bytes do arquivo de entrada
+    ler_cabecalho:
     mov eax, 3 ; Ler arquivo de entrada
-    mov ebx, [fileHandle]
+    mov ebx, dword [fileHandleIn]
     mov ecx, fileBuffer
-    mov edx, 3  ; Largura de 3 bytes por pixel
+    mov edx, 10
     int 80h
 
-    ; Escrever os 32 bytes no arquivo de saída
-    mov eax, 4
-    mov ebx, [fileHandle]
-    mov ecx, buffer
-    mov edx, 32
+    ; Passo 5: Escrever os primeiros 54 bytes no arquivo de saída
+    escrever_cabecalho:
+    mov eax, 4 ; Escrever no arquivo de saída
+    mov ebx, dword [fileHandleOut]
+    mov ecx, fileBuffer
+    mov edx, 10
     int 80h
- 
-    mov ebx, fileBuffer  ; ebx aponta para os bytes da imagem
-    mov edx, [integer_x]  ; Coordenada X inicial
-    mov esi, [integer_y]  ; Coordenada Y inicial
-    mov edi, [integer_width]  ; Largura da censura
+
+    ; Lógica de censura - suponha que as coordenadas e dimensões sejam válidas
+    censurar_retangulo:
+    mov edi, [integer_x]      ; Coordenada X inicial
+    mov esi, [integer_y]      ; Coordenada Y inicial
+    mov edx, [integer_width]  ; Largura da censura
     mov ebp, [integer_height] ; Altura da censura
- 
-            
-    ;censurando linha por linha
+
+    ; Loop para censurar linha por linha
     linha_loop:
-    mov eax, 3
-    mov ebx, [fileHandle]
-    mov ecx, fileBuffer 
+    cmp esi, [integer_y]         ; Verifique se estamos dentro do retângulo na coordenada Y
+    jl pular_censura
+    cmp esi, [integer_y]         ; Verifique se estamos dentro do retângulo na coordenada Y
+    jg pular_censura
+    cmp edi, [integer_x]         ; Verifique se estamos dentro do retângulo na coordenada X
+    jl pular_censura
+    cmp edi, [integer_x]         ; Verifique se estamos dentro do retângulo na coordenada X
+    jg pular_censura
 
-    mov edx, [integer_width]
-    mov esi, 3
-    mov edi, 0 
-     
-    ;censurar_linha:
-    ;mov byte [ecx], 0  ; Censura o componente azul
-    ;mov byte [ecx + 1], 0  ; Censura o componente verde
-    ;mov byte [ecx + 2], 0  ; Censura o componente vermelho
-    ;add ecx, 3  ; Avança para o próximo pixel
-    ;sub edx, 1  ; Decrementa a largura da censura
-    ;cmp edx, 0
-    ;je linha_loop
-    ;jmp censurar_linha
+    ; Se estamos dentro do retângulo, censure o pixel
+    mov eax, 3 ; Ler um pixel da imagem de entrada
+    mov ebx, [fileHandleIn]
+    mov ecx, fileBuffer
+    mov edx, 3  ; Tamanho de um pixel em bytes (neste caso, supomos que cada pixel tem 3 bytes)
+    int 80h
 
-    censurar_linha:
-    mov byte [ebx], 0  ; Censura o componente azul
-    mov byte [ebx + 1], 0  ; Censura o componente verde
-    mov byte [ebx + 2], 0  ; Censura o componente vermelho
-    add ebx, 3  ; Avança para o próximo pixel
-    sub edx, 1  ; Decrementa a largura da censura
-    cmp edx, 0
-    je linha_loop
-    jmp censurar_linha
+    ; Agora, você deve censurar este pixel (definindo os valores dos componentes de cor para 0)
+    mov byte [ecx], 0  ; Censura o componente azul
+    mov byte [ecx + 1], 0  ; Censura o componente verde
+    mov byte [ecx + 2], 0  ; Censura o componente vermelho
 
+    ; Escrever o pixel censurado na imagem de saída
+    mov eax, 4 ; Escrever um pixel na imagem de saída
+    mov ebx, [fileHandleOut]
+    mov ecx, fileBuffer
+    mov edx, 3  ; Tamanho de um pixel em bytes
+    int 80h
 
-add esi, 1  ; Avança para a próxima linha
-sub ebp, 1  ; Decrementa a altura da censura
-cmp ebp, 0
-je fim
-jmp linha_loop
+    pular_censura:
+    ; Atualize as coordenadas e dimensões conforme necessário
+    add edi, 1  ; Avança para o próximo pixel na linha
+    cmp edi, [integer_x]         ; Verifique se estamos dentro do retângulo na coordenada X
+    jl linha_loop ; Se ainda não saímos do retângulo, continue
+    add esi, 1  ; Avança para a próxima linha
+    sub ebp, 1  ; Decrementa a altura da censura
+    cmp ebp, 0
+    je fechar_arquivos ; Se a altura da censura atingiu zero, saia do loop
+    jmp censurar_retangulo
 
+    fechar_arquivos:
+    mov eax, 6 ; Fechar o arquivo de entrada
+    mov ebx, dword [fileHandleIn]
+    int 80h
 
+    mov eax, 6 ; Fechar o arquivo de saída
+    mov ebx, dword [fileHandleOut]
+    int 80h
 
-fechar_arquivo:
-mov eax, 6
-mov ebx, [fileHandle]
-int 80h
-
-
-fim: 
-mov eax, 1
-xor ebx, ebx
-int 80h
+    fim: 
+    mov eax, 1
+    xor ebx, ebx
+    int 80h
